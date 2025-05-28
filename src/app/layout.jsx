d3cfbdcc9ff87fdf42fbe4acca97cf8a7ea8ceae
elvxk <pradiptaramadan9c@gmail.com>
2025-05-28 09:31:32 +0700

    fix(ico): fixing favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,21 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-96x96.png"
-          sizes="96x96"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
         <meta name="apple-mobile-web-app-title" content="Pradipta" />
-        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`antialiased bg-cbrown dark:bg-cdark transition-all`}>
         {children}
